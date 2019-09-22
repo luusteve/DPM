12015545df2c41ba923ded94600bcdc8cc3c596e
Hassan Haidar <hassan.haidar@mail.mcgill.ca>
2019-09-22 13:11:48 -0400

    implement odometry correction :sparkles:

--- Group33_Lab2_Haidar_Luu/src/ca/mcgill/ecse211/lab2/OdometryCorrection.java
+++ Group33_Lab2_Haidar_Luu/src/ca/mcgill/ecse211/lab2/OdometryCorrection.java
@@ -2,19 +2,32 @@ package ca.mcgill.ecse211.lab2;
 
 import static ca.mcgill.ecse211.lab2.Resources.*;
 import lejos.hardware.Sound;
+import lejos.hardware.sensor.SensorModes;
 import lejos.robotics.SampleProvider;
 
 public class OdometryCorrection implements Runnable {
   private static final long CORRECTION_PERIOD = 10;
   private static final double LIGHTSENSOR_THRESHOLD = 40;
+  
+//  recommended by TA
+  private static final double LIGHTSENSOR_DELTA = 25;
+  
   private SampleProvider light = colorSensor.getRedMode();
   private float[] lightData = new float[colorSensor.sampleSize()];
   private int lightValue;
 
+  // added by Hassan
+  Odometer odometer  = Odometer.getOdometer();
+  private int INITIAL_LIGHT;
+  
+  
+  
   public boolean correctionTrigger() {
     light.fetchSample(lightData, 0);
     lightValue = (int) (lightData[0] * 100);
-    if (lightValue < LIGHTSENSOR_THRESHOLD)
+    //if (lightValue < LIGHTSENSOR_THRESHOLD)
+    //  return true;
+    if (Math.abs(lightValue - INITIAL_LIGHT) > LIGHTSENSOR_DELTA)
       return true;
     return false;
   }
@@ -24,12 +37,47 @@ public class OdometryCorrection implements Runnable {
    */
   public void run() {
     long correctionStart, correctionEnd;
+    
+    // initial color reading
+    light.fetchSample(lightData, 0);
+    INITIAL_LIGHT = (int) (lightData[0] * 100);
+    
+    
+    int tileCountX = 0;
+    int tileCountY = 0;
     while (true) {
       correctionStart = System.currentTimeMillis();
       if (correctionTrigger()) {
         Sound.beep();
+        
+        double theta = odometer.getXYT()[2];
+        // moving in positive y
+        if (theta <= 20 || theta >= 340) {
+          tileCountY++;
+//        odometer.update(0,TILE_SIZE * tileCountY-odometer.getXYT()[1] , 0);
+          odometer.setY(TILE_SIZE * tileCountY);
+        }
+        // moving in positive x
+        else if (theta <= 110 && theta >= 80) {
+          tileCountX++;
+          odometer.setX(TILE_SIZE * tileCountX);
+        }
+        //moving in negative y
+        else if (theta <= 200 && theta >= 160) {
+         
+          odometer.setY(TILE_SIZE * tileCountY);
+          tileCountY--;
+        }
+        // moving in negative x
+        else if(theta <= 290 && theta > 250) {
+       
+          odometer.setX(TILE_SIZE * tileCountX);
+          tileCountX--;
+        }
       }
       // TODO Trigger correction (When do I have information to correct?)
+      
+      
       // colorSensor.set
       // TODO Calculate new (accurate) robot position
 
